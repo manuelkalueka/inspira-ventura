9deb167015785de5614271fcbf4b9907084047f3
Manuel Kalueka <pedrokalueca@gmail.com>
2023-10-06 20:07:49 +0100

    Menu drop Feito com javascript simples

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,29 @@ function hideLoader() {
 }
 
 hideLoader();
+
+function showDropMenu(container, menu, evento) {
+
+    switch (evento) {
+        case 'mouseover':
+            container.addEventListener(evento, () => {
+                menu.classList.remove('hide')
+            })
+            break;
+
+        case 'mouseout':
+            container.addEventListener(evento, () => {
+                menu.classList.add('hide')
+            })
+            break;
+
+        default:
+            throw new Error('Funcionalidade apenas com mouseover e mouseout');
+    }
+}
+
+const dropMenu = document.querySelector('.drop-menu');
+const dropMenuTarget = document.querySelector('.drop-container');
+
+showDropMenu(dropMenu, dropMenuTarget, 'mouseover');
+showDropMenu(dropMenu, dropMenuTarget, 'mouseout');

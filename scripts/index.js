bbf835aad671b33815f3e7d3d379f2889507b46f
Manuel Kalueka <pedrokalueca@gmail.com>
2023-10-13 13:07:22 +0100

    Função Back top feito

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 function hideLoader() {
     document.addEventListener("DOMContentLoaded", () => {
         const loaderContainer = document.querySelector(".loader-container");
+        document.body.style.overflow = "hidden";
         // Remova o loader após o conteúdo ser totalmente carregado
         setTimeout(() => {
             loaderContainer.style.display = "none";
-        }, 1000);
+            document.body.style.overflow = "visible";
+        }, 2000);
     });
 }
 
@@ -26,7 +28,7 @@ function showDropMenu(container, menu, evento) {
             break;
 
         default:
-            throw new Error('Funcionalidade apenas com mouseover e mouseout');
+            console.error('Drop menu desabilitado')
     }
 }
 
@@ -35,3 +37,40 @@ const dropMenuTarget = document.querySelector('.drop-container');
 
 showDropMenu(dropMenu, dropMenuTarget, 'mouseover');
 showDropMenu(dropMenu, dropMenuTarget, 'mouseout');
+
+
+const check = document.querySelector('#check-menu');
+
+check.addEventListener('change', () => {
+    if (check.checked) {
+        document.querySelector('.menu-nav').style.display = 'block';
+    } else {
+        document.querySelector('.menu-nav').style.display = 'none';
+    }
+})
+
+
+function showScrollButton(mybutton) {
+    const valorScroll = 50;
+
+    if (document.body.scrollTop > valorScroll || document.documentElement.scrollTop > valorScroll) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// Quando o usuário clica no botão, rola para o topo da página
+function backToTopScroll() {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
+}
+
+const btnTop = document.querySelector('#btn-top')
+
+window.onscroll = () => {
+    showScrollButton(btnTop);
+};
+
+btnTop.addEventListener('click', backToTopScroll)
+
